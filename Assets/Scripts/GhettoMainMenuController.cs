cd0a375e70e32855c4f913c9395aae4d95d2cd6c
Zack Rock <zacharyrock@hotmail.com>
2017-07-16 15:50:51 -0700

    Splash screen.

---
+++ Assets/Scripts/GhettoMainMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhettoMainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.Find("DustTrail_0").GetComponent<SpriteRenderer>().enabled = false;
+		GetComponentInChildren<Animator>().Play( Animator.StringToHash( "Idle" ) );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
